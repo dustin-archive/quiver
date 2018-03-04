0d78e5a1b1eb39fc3654ff56920f8c166fa256a6
Dustin Dowell <dustindowell22@gmail.com>
2018-03-03 20:05:59 -0600

    organize

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 
 'use strict'
 
-import { h, app } from 'hyperapp'
-
-import Bar from './components/Bar'
+import { app as hyperapp } from 'hyperapp'
 
 import RouterInit from './main/RouterInit'
 
 import Router from './stores/Router'
 import RouterPage from './stores/RouterPage'
 
-import RouterView from './views/RouterView'
+import App from './views/App'
 
 const state = {
   Router: {},
@@ -24,24 +22,10 @@ const actions = {
   update: data => data
 }
 
-const view = (...args) =>
-  h('div', { class: 'app' }, [
-    h('div', { class: 'app-content' }, [
-      h('div', { class: 'app-main' }, [
-        h('div', { class: 'app-main-inner-left' }),
-        h('div', { class: 'app-main-inner-right' }, [
-          RouterView(args)
-        ])
-      ]),
-      h('div', { class: 'app-side' }, [
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      ])
-    ]),
-    Bar(args)
-  ])
+const view = (...args) => App(args)
 
 // create app instance
-const main = app(state, actions, view, document.getElementById('hyperapp'))
+const main = hyperapp(state, actions, view, document.getElementById('hyperapp'))
 
 RouterInit(main)
 
---
+++ src/views/App.js
@@ -0,0 +1,23 @@
+
+import { h } from 'hyperapp'
+
+import Bar from './Bar'
+import RouterView from './RouterView'
+
+const App = args =>
+  h('div', { class: 'app' }, [
+    h('div', { class: 'app-content' }, [
+      h('div', { class: 'app-main' }, [
+        h('div', { class: 'app-main-inner-left' }),
+        h('div', { class: 'app-main-inner-right' }, [
+          RouterView(args)
+        ])
+      ]),
+      h('div', { class: 'app-side' }, [
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      ])
+    ]),
+    Bar(args)
+  ])
+
+export default App
