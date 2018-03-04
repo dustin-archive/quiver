817f5246d1f78eaa6a3d0e6918949a0d30cd8dd4
Dustin Dowell <dustindowell22@gmail.com>
2018-03-03 22:52:04 -0600

    gems

--- src/views/Bar.js
+++ src/views/Bar.js
@@ -4,7 +4,7 @@ import { h } from 'hyperapp'
 const Bar = args =>
   h('div', { class: 'bar' }, [
     h('div', { class: 'bar-inner' }, [
-      h('img', { src: 'images/hyperapp.png' })
+      h('img', { src: 'images/logo.png' })
     ])
   ])
 
