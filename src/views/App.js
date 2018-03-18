d0ecb45823b3f77203470818a1db3696ea90f063
Dustin Dowell <dustindowell22@gmail.com>
2018-03-18 06:41:16 -0500

    major layout simplification

--- src/views/App.js
+++ src/views/App.js
@@ -6,19 +6,15 @@ import RouterView from './RouterView'
 
 const App = args =>
   h('div', { class: 'app' }, [
-    h('div', { class: 'app-content' }, [
-      h('div', { class: 'app-main' }, [
-        h('div', { class: 'app-inner' }, [
-          RouterView(args),
-          Bar(args)
-        ])
-      ]),
-      h('div', { class: 'app-side' }, [
-        'cart items',
-        'filters',
-        'personal lists'
-        // 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      ])
+    h('div', { class: 'app-main' }, [
+      RouterView(args),
+      Bar(args)
+    ]),
+    h('div', { class: 'app-side' }, [
+      'cart items',
+      'filters',
+      'personal lists'
+      // 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     ])
   ])
 
--- src/views/Bar.js
+++ src/views/Bar.js
@@ -5,8 +5,8 @@ import Control from './Control'
 
 const Bar = args =>
   h('div', { class: 'bar' }, [
-    h('div', { class: 'bar-inner' }, [
-      h('a', { href: '/' }),
+    h('div', { class: 'bar-main' }, [
+      h('a', { class: 'bar-logo', href: '/' }),
       Control(args)
     ])
   ])
